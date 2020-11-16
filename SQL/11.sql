116d70ccd93f6ba1645dfdba0072301058e429ae
chauda1 <50472636+chauda1@users.noreply.github.com>
2020-11-16 13:21:56 -0800

    11

--- SQL/11.sql
+++ SQL/11.sql
@@ -1,7 +1,10 @@
-SELECT P.name, P.age 
+SELECT P.primaryName, P.age, Count(*)
 FROM person AS P, rating AS R, principals AS Pr, title AS T
 WHERE R.averageRating > 5
 AND T.titleID = R.titleID
-AND T.titleType LIKE "movie"
+AND T.titleType = "movie"
 AND Pr.titleID = T.titleID
-AND Pr.category LIKE "actor"
+AND (Pr.category = "actor" OR Pr.category = "actress")
+AND P.personID = Pr.personID
+GROUP BY P.personID
+HAVING COUNT(*) > 2
